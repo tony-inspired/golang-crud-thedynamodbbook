ceae05e38a5a838e7e31f2cbb8abbba25bc37f4d
Anton Starostin <anton.starostin@bd.com>
2024-01-19 10:01:07 +0200

    code clean up

--- utils/env/env.go
+++ utils/env/env.go
@@ -2,14 +2,12 @@ package env
 
 import "os"
 
-// get env
-// todo: rename to GetEnvVariable
+
 func GetEnv(env, defaultValue string) string {
 	environment := os.Getenv(env)
 	if environment == "" {
-		//return default
 		return defaultValue
 	}
-	//return environment
+
 	return environment
 }
